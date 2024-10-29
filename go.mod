ce01eba67ebce8a3c816a927c98017a8248ddcfd
iaoongin <28687951+iaoongin@users.noreply.github.com>
2024-10-29 06:03:37 +0000

    Support save data to db

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module yffjglcms.com/note-go
 
 go 1.23.2
 
-require github.com/gin-gonic/gin v1.10.0
+require (
+	github.com/gin-gonic/gin v1.10.0
+	github.com/syndtr/goleveldb v1.0.0
+)
 
 require (
 	github.com/bytedance/sonic v1.11.6 // indirect
@@ -15,6 +18,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.20.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/golang/snappy v0.0.0-20180518054509-2e65f85255db // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
--- main.go
+++ main.go
@@ -2,16 +2,13 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"math/rand"
 	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
 )
-
-type TextData struct {
-	Text string
-}
 
 func main() {
 	r := gin.Default()
@@ -20,25 +17,43 @@ func main() {
 	r.LoadHTMLGlob("templates/*")
 
 	// 用于存储文本数据的map
-	textDataMap := make(map[string]*TextData)
+	// textDataMap := make(map[string]*TextData)
+
+	// 使用db
+	// 创建 LevelDBWrapper 实例
+	dbWrapper, err := NewLevelDBWrapper("data/notes.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer dbWrapper.Close()
 
 	r.GET("/", func(c *gin.Context) {
 		// 生成一个随机地址并重定向
 		randomAddress := generateRandomAddress()
-		textDataMap[randomAddress] = &TextData{
+		// textDataMap[randomAddress] = &TextData{
+		// 	Text: "Hello, World!",
+		// }
+
+		// 插入数据
+		if err := dbWrapper.Put(randomAddress, TextData{
 			Text: "Hello, World!",
+		}); err != nil {
+			log.Fatal(err)
 		}
+
 		c.Redirect(http.StatusFound, "/"+randomAddress)
 	})
 
 	r.GET("/:address", func(c *gin.Context) {
 		address := c.Param("address")
-		if textData, ok := textDataMap[address]; ok {
+		// 查询数据
+		if textData, err := dbWrapper.Get(address); err == nil {
 			c.HTML(http.StatusOK, "index.html", gin.H{
 				"RandomAddress": address,
 				"Text":          textData.Text,
 			})
 		} else {
+			log.Fatal(err)
 			// 如果没有找到对应的文本数据,则显示404页面
 			c.HTML(http.StatusNotFound, "404.html", nil)
 		}
@@ -49,9 +64,18 @@ func main() {
 		// 获取提交的文本内容
 		text := c.PostForm("text")
 		// 保存文本内容
-		textDataMap[address] = &TextData{
+
+		// textDataMap[address] = &TextData{
+		// 	Text: text,
+		// }
+
+		// 插入数据
+		if err := dbWrapper.Put(address, &TextData{
 			Text: text,
+		}); err != nil {
+			log.Fatal(err)
 		}
+
 		fmt.Println("Saved text:", text, "at address:", address)
 		c.Redirect(http.StatusFound, "/"+address)
 	})
